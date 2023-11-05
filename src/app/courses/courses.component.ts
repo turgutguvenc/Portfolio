96995c8bd1fd3bbd3e8c2edef86126a4afb47425
turgutguvenc <turgutguvenc@icloud.com>
2023-11-05 01:29:07 +0100

    Course Section is improved

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent {
+  isButtonClicked:boolean = false;
 
+  changeFrame()
+  {
+    this.isButtonClicked = !this.isButtonClicked;
+  }
 }
