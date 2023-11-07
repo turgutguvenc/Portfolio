d541cc8226acd7fe45a15d1c28623910cd12b65b
turgutguvenc <turgutguvenc@icloud.com>
2023-11-07 22:35:06 +0100

    Courses page is ready for all functionalities

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,13 +3,64 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent {
-  isButtonClicked:boolean = false;
+  isFirstButtonClicked: boolean = false;
+  isSecondButtonClicked: boolean = false;
+  isThirdButtonClicked: boolean = false;
+  isFourthButtonClicked: boolean = false;
+  isFifthButtonClicked: boolean = false;
+  isSixthButtonClicked: boolean = false;
+  isSeventhButtonClicked: boolean = false;
+  isEighthButtonClicked: boolean = false;
+  isNinethButtonClicked: boolean = false;
+  isTenthButtonClicked: boolean = false;
+  isEleventhButtonClicked: boolean = false;
+  isTwelvethButtonClicked: boolean = false;
+  isThirteenButtonClicked: boolean = false;
+  isFourteenButtonClicked: boolean = false;
 
-  changeFrame()
-  {
-    this.isButtonClicked = !this.isButtonClicked;
+  changeFrame() {
+    this.isFirstButtonClicked = !this.isFirstButtonClicked;
+  }
+  changeFrameGans() {
+    this.isSecondButtonClicked = !this.isSecondButtonClicked;
+  }
+  changeFrameDeep() {
+    this.isThirdButtonClicked = !this.isThirdButtonClicked;
+  }
+  changeFrameGans2() {
+    this.isFourthButtonClicked = !this.isFourthButtonClicked;
+  }
+  changeFramePowerBI() {
+    this.isFifthButtonClicked = !this.isFifthButtonClicked;
+  }
+  changeFrameSql() {
+    this.isSixthButtonClicked = !this.isSixthButtonClicked;
+  }
+  changeFrameMit() {
+    this.isSeventhButtonClicked = !this.isSeventhButtonClicked;
+  }
+  changeFramePhyton() {
+    this.isEighthButtonClicked = !this.isEighthButtonClicked;
+  }
+  changeFrameMachine() {
+    this.isNinethButtonClicked = !this.isNinethButtonClicked;
+  }
+  changeFrameNeural() {
+    this.isTenthButtonClicked = !this.isTenthButtonClicked;
+  }
+  changeFrameNeuralImproving() {
+    this.isEleventhButtonClicked = !this.isEleventhButtonClicked;
+  }
+  changeFrameNeuralStructuring() {
+    this.isTwelvethButtonClicked = !this.isTwelvethButtonClicked;
+  }
+  changeFrameNeuralConvolution() {
+    this.isThirteenButtonClicked = !this.isThirteenButtonClicked;
+  }
+  changeFrameNeuralGANS(){
+    this.isFourteenButtonClicked = !this.isFourteenButtonClicked;
   }
 }
