fd3372dc55a8413ac4c01235c0cddb06995f5ec0
turgutguvenc <turgutguvenc@icloud.com>
2023-12-06 00:05:02 +0100

    Production

--- src/app/components/cv/cv.component.ts
+++ src/app/components/cv/cv.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.css']
+  styleUrls: ['./cv.component.css'],
 })
 export class CvComponent {
-
+  // tslint:disable-next-line: no-inferrable-types
 }
